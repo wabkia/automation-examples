972f6904261d6cac5c4d74c9e13132c73cc31f00
Richard Casey <richard.casey@locationlabs.com>
2019-01-10 04:23:31 -0800

    added argument support, step direction check, and module support to cracklepop.py

--- cracklepop.py
+++ cracklepop.py
@@ -1,11 +1,35 @@
 
-def crackiterable(start=1, end=100):
+from sys import argv as argv
+
+# attempt to set command line arguments as start and end values
+try:
+    start = int(argv[1])
+    end = int(argv[2])
+except:
+    print("There is a problem with the supplied arguments")
+raise
+
+# determine the direction of the count
+def stepdirection(start, end):
+    if start > end:
+        end -= 1
+        step = -1
+    else:
+        end += 1
+        step = 1
+    return start, end, step
+
+
+# generate list using default values or supplied arguments
+# then iterate over the list, checking for snap, crackle,
+# and cracklepop conditions.
+def crackiterable(start=-1, end=-100):
     try:
         totalcount = 0
         cracklepopcount = 0
         snapcount = 0
         cracklecount = 0
-        for number in range(start, (end+1)):
+        for number in range(*stepdirection(start, end)):
             totalcount += 1
             if number % 3 == 0 and number % 5 == 0:
                 print("{0} - Cracklepop!\n").format(number)
@@ -30,5 +54,14 @@ def crackiterable(start=1, end=100):
         print("Unexpected error!")
         raise
 
+# if called from the command line, attempt to use arguments
+# and if no arguments exist call function with defaults
+if __name__ == "__main__":
+    if 'start' in globals() and 'end' in globals():
+        crackiterable(start, end)
+    else:
+        crackiterable()
 
-crackiterable()
+
+
+
